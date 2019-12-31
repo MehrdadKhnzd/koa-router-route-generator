01a9ddccab4ee3b2befd27c27fe2a591816a12bb
MehrdadKhnzd <mehrdadkhnzd@gmail.com>
2019-12-31 22:02:29 +0330

    feat: add route as object support (name, handler, children)

--- index.js
+++ index.js
@@ -11,10 +11,30 @@ const routeFlattener = (routes, path='') => {
   return flattenedRoutes;
 }
 
+const routeGenerator = (router, routeGroups) => {
   (Array.isArray(routeGroups) ? routeGroups : [routeGroups]).forEach(routeGroup => {
     Object.keys(routeGroup).forEach(method => {
-      Object.keys(routeGroup[method]).forEach(route => {
-        router[method](...[route, ...(Array.isArray(routeGroup[method][route]) ? routeGroup[method][route] : [routeGroup[method][route]])]);
+      const flattenedRoutes = routeFlattener(routeGroup[method]);
+      Object.keys(flattenedRoutes).forEach(route => {
+        if (Array.isArray(routeGroup[method][route]) && routeGroup[method][route].length !== 0) {
+          router[method](route, ...routeGroup[method][route]);
+        } else if (typeof routeGroup[method][route] === 'function') {
+          router[method](route, routeGroup[method][route]);
+        } else {
+          if (Array.isArray(routeGroup[method][route][handler]) && routeGroup[method][route][handler].length !== 0) {
+            if (routeGroup[method][route][name]) {
+              router[method](routeGroup[method][route][name], router, ...routeGroup[method][route])
+            } else {
+              router[method](router, ...routeGroup[method][route])
+            }
+          } else if (typeof routeGroup[method][route][handler] === 'function') {
+            if (routeGroup[method][route][name]) {
+              router[method](routeGroup[method][route][name], router, routeGroup[method][route])
+            } else {
+              router[method](router, routeGroup[method][route])
+            }
+          }
+        }
       });
     })
   });
