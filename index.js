e93a86d0f08b00d2b44be0000bbafb57d5f9e7fd
MehrdadKhnzd <mehrdadkhnzd@gmail.com>
2019-12-31 22:00:24 +0330

    feat: add route flattener

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-module.exports = (router, routeGroups) => {
+const routeFlattener = (routes, path='') => {
+  let flattenedRoutes = {};
+  Object.keys(routes).map(route => {
+    const children = routes[route].children;
+    delete routes[route].children;
+    flattenedRoutes[path + route] = routes[route];
+    if (children) {
+      Object.assign(flattenedRoutes, routeFlattener(children, path + route));
+    }
+  });
+  return flattenedRoutes;
+}
+
   (Array.isArray(routeGroups) ? routeGroups : [routeGroups]).forEach(routeGroup => {
     Object.keys(routeGroup).forEach(method => {
       Object.keys(routeGroup[method]).forEach(route => {
